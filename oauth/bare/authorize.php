b4c52292c56163ed009ec84adfdec1314dba7274
Paul Tarjan <github@paulisageek.com>
2012-05-30 18:36:58 -0700

    I have to handle ? in the url. God this is so hacky...

--- oauth/bare/authorize.php
+++ oauth/bare/authorize.php
@@ -1,3 +1,13 @@
 <?php
 
-header('Location: '.$_REQUEST['redirect_uri'].'?code=1234');
+$url = $_GET['redirect_uri'];
+$has_query = strpos($url, '?');
+$code = 'code=1234';
+
+if ($has_query) {
+  $url = str_replace('?', '?'.$code.'&', $url);
+} else {
+  $url = $url.'?'.$code;
+}
+
+header('Location: '.$url);
