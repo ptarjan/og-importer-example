a347e5299468884b372ffc435211fa6d273b9c5a
Paul Tarjan <github@paulisageek.com>
2012-06-04 18:03:36 -0700

    access token params should be POST

--- oauth/memcache/access_token.php
+++ oauth/memcache/access_token.php
@@ -6,12 +6,12 @@ require_once('memcache.php');
 $user_id = get_loggedin_user();
 $m = get_memcache();
 
-assert_correct_secret($_GET['client_id'], $_GET['client_secret']);
+assert_correct_secret($_POST['client_id'], $_POST['client_secret']);
 
 $key = 'oauth:code:'.$user_id.':'.$_POST['client_id'].':'.$_POST['redirect_uri'];
 $code = $m->get($code_key);
 
-if ($code != $_GET['code']) {
+if ($code != $_POST['code']) {
   die(
     json_encode(
       array(
@@ -31,7 +31,7 @@ $m->set(
   json_encode(
     array(
       'user' => $user_id, 
-      'app' => $_GET['client_id'],
+      'app' => $_POST['client_id'],
     )
   )
 );
